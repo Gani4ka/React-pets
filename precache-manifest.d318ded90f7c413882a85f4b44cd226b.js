fe1ea0b14761e810fe3d9b65a35706de3e5a9d6e
Anya Efimova <gani4ka_n@ukr.net>
2019-07-25 00:39:26 +0300

    Updates

---
+++ precache-manifest.d318ded90f7c413882a85f4b44cd226b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62450c96d1c4a75f8a3c764d675a736b",
+    "url": "/react-hw3/index.html"
+  },
+  {
+    "revision": "68b0697d9e31cd166b59",
+    "url": "/react-hw3/static/css/main.67ad09c3.chunk.css"
+  },
+  {
+    "revision": "251c9f0fa02186b15272",
+    "url": "/react-hw3/static/js/2.dbe413bb.chunk.js"
+  },
+  {
+    "revision": "68b0697d9e31cd166b59",
+    "url": "/react-hw3/static/js/main.9037f70f.chunk.js"
+  },
+  {
+    "revision": "53cd92bbd5638358f3b3",
+    "url": "/react-hw3/static/js/runtime~main.1195c195.js"
+  }
+]);
