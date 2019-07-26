1e1dc67b0c987e64f7cb13ac62225a17dd0a39b8
Anya Efimova <gani4ka_n@ukr.net>
2019-07-27 00:00:25 +0300

    fix for gh-pages

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter basename="/">
     <Route component={App} />
-  </BrowserRouter>,
+  </HashRouter>,
   document.querySelector('#root'),
 );
